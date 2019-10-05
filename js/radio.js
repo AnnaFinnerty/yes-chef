a4cf10ff7c09ffc014493f7f54dd770fea2aea6f
AnnaFinnerty <algfinnerty@gmail.com>
2019-10-05 15:01:58 -0600

    class Radio buildout

--- js/radio.js
+++ js/radio.js
@@ -1,3 +1,16 @@
 class Radio{
-
+    constructor(){
+        this.subscribers = {}
+    }
+    addSubscriber(subcriberName, subscriberCallback){
+        //add a subcriber to list of subscribers
+        if(!this.subscribers[subcriberName]){
+            this.subscribers[subcriberName] = subscriberCallback;
+        }
+    }
+    callSubcriber(subcriberName,message){
+        //sends a message to a subscriber
+        const callback = this.subscribers[subcriberName];
+        callback(message);
+    }
 }
