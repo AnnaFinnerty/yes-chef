5f5d2695e59bac2ae08fea4481b5681ff3db969a
AnnaFinnerty <algfinnerty@gmail.com>
2019-10-05 12:51:17 -0600

    class Restaurant buildout

--- js/restuarant.js
+++ js/restuarant.js
@@ -1,5 +1,37 @@
 class Restuarant{
-    constructor(){
+    constructor(name,style,tables){
+        //these parameters must exist!
+        this.name = null;
+        //style of food
+        this.style = null;
+        this.tables = {};
+
+        this.profit = 0;
+        this.rating = 0;
+        this.incomeHistoryDaily = [];
+        this.incomeHistoryMonthly = [];
+
+        //decor properties
+        this.decor = {
+            
+            floor: {
+                type: "board",
+                color: "brown",
+            },
+            walls:{
+                type: "wallpaper1",
+                color: "blue",
+            }
+
+        }
+    }
+    profit(amt){
+        this.profit += amt;
+    }
+    loss(amt){
+        this.profit -= amt;
+    }
+    validator(){
         
     }
 }
