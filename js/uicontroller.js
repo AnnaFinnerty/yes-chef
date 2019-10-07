b731a821f2b4c72a89720c215357c097649edf7c
AnnaFinnerty <algfinnerty@gmail.com>
2019-10-07 09:12:06 -0600

    final UIController commit

--- js/uicontroller.js
+++ js/uicontroller.js
@@ -27,6 +27,9 @@ class UIController{
         this.tabsTracker = {
             ingrediantTabs: {
                 active: null,
+                container: this.displayElements.$ingrediantsTabs,
+                elements: null,
+                elementStyle: "ingrediants-tab",
                 data: null,
                 dataLoc: "Ingrediants",
                 dataCommand: "returnIngrediantCategories",
@@ -44,15 +47,20 @@ class UIController{
             this.updateTabs(i);
         }
     }
-    updateTabs(tabsName){
-        //get tab info if not loaded
+    updateTabs(tabsName,selectedPosition){
         console.log(tabsName);
         if(this.tabsTracker[tabsName]){
             if(this.tabsTracker[tabsName]['active'] === null){
-                //request tabs information from module that holds it
+                //request tab info if not already loaded
                 this.radio.callSubscriber(this.tabsTracker[tabsName]['dataLoc'],{command:this.tabsTracker[tabsName]['dataCommand'],return:"UIController"});
             } else {
                 console.log("time to make tabs!");
+                if(this.tabsTracker[tabsName]['elements'] === null){
+                    //if tab elements are not built, build and store them
+                    const tab = $('<div/>');
+                } else {
+
+                }
             }
         }
     }
